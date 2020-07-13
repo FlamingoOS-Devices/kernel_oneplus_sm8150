baccece25519378b7f434b17b0fe0e188b15a10a
grislux55 <jebchou.hg@gmail.com>
2020-07-13 17:07:24 +0800

    drm: Set proper value if fp mode on
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5972,6 +5972,14 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 		return 0;
 	}
 
+	if (fp_mode == 1) {
+		display->panel->dim_status = true;
+		cstate->fingerprint_pressed = true;
+	} else if (fp_mode == 0) {
+		display->panel->dim_status = false;
+		cstate->fingerprint_pressed = false;
+	}
+
 	return 0;
 }
 
